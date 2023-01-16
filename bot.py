5f4c483c6bbaa8ed284cf5c51dad0eaea638321f
biggestoh <jamieowensmusic@gmail.com>
2023-01-16 21:23:29 +0000

    I updated loads of stuff

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 #!/bin/python3
 import discord
 from discord.ext import commands
+import json
+from random import choice
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -10,9 +12,15 @@ client = commands.Bot(command_prefix='$', intents=intents)
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
+    quotes = json.load(open('quotes.json'))
+
 
 @client.command()
 async def hello(ctx):
     await ctx.send("hello")
+
+@client.command()
+async def quote(ctx):
+    await ctx.send(quote.choice())
 
 client.run('MTA2NDYzNDI5MTA3OTg4MDc1NA.Ga9c92.VP3Y1bY8PUZkKFv4RCAgfRRfeCU1byWAy9GStc')
