022602672196743e730c69edd195b4d060688a29
Pim Nelissen <pim.nelissen@protonmail.com>
2023-01-16 21:56:24 +0100

    initial version + quotes

---
+++ bot.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = commands.Bot(command_prefix='$', intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.command()
+async def hello(ctx):
+    await ctx.send("hello")
+
+client.run('MTA2NDYzNDI5MTA3OTg4MDc1NA.Ga9c92.VP3Y1bY8PUZkKFv4RCAgfRRfeCU1byWAy9GStc')
