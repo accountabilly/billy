cf3fd6f02ea3656338227af6c31bc11bd88bd7cb
biggestoh <98284120+biggestoh@users.noreply.github.com>
2023-03-30 22:37:23 +0100

    Formatting and readability

--- bot.py
+++ bot.py
@@ -11,8 +11,9 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = commands.Bot(command_prefix='$', intents=intents)
 
-# TODO: move sensitive data to git ignore file
+# TODO: move sensitive data to .gitignore file
 DISCORD_TOKEN = 'MTA2NDYzNDI5MTA3OTg4MDc1NA.Ga9c92.VP3Y1bY8PUZkKFv4RCAgfRRfeCU1byWAy9GStc'
+DATABASE = "data/billy.db"
 GIT_USER = "pim-wtf"
 GIT_REPO = "billy"
 GIT_TOKEN = "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"
@@ -38,22 +39,22 @@ async def send_dm(user_id, message):
 
 
 async def fetch_data(user_id, *columns):
+    """
 
-    # TODO: Comments
+    :param user_id:
+    :param columns:
+    :return:
+    """
 
     # Connect to database
-
-    async with aiosqlite.connect("data/billy.db") as db:
+    async with aiosqlite.connect(DATABASE) as db:
         cursor = await db.cursor()
 
-        if columns:
-
-            # If columns are specified, only fetch those columns. Use column names provided in *columns
+        if columns:  # Get specified columns
             column_names = list(columns)
             query = f"SELECT {','.join(column_names)} FROM user_data WHERE user_id={user_id};"
 
-        else:
-            # If no columns are specified, fetch all columns and all column names.
+        else:  # Get all columns
             query = f"SELECT * FROM user_data WHERE user_id={user_id};"
             await cursor.execute(query)
             column_names = [description[0] for description in cursor.description]
@@ -62,18 +63,21 @@ async def fetch_data(user_id, *columns):
         await cursor.execute(query)
         row = await cursor.fetchone()
 
-        if row:
-
+        if row:  # If data was fetched then format data into a database where column name is the key
             data = {}
             for i, value in enumerate(row):
+
                 # Check if the value is a string that looks like a list and parse it
                 if isinstance(value, str) and value.startswith("[") and value.endswith("]"):
                     value = ast.literal_eval(value)
                 data[column_names[i]] = value
+
+            # Delete 'user_id' key value pair as not required and return data
             if 'user_id' in data:
                 del data['user_id']
             return data
-        else:
+
+        else:  # If no data fetched from database then return None
             return None
 
 
@@ -101,96 +105,109 @@ async def quote(ctx):
 
 @client.command()
 async def add_habit(ctx, *, habit=None):
-    # TODO: Comments
+    # Comment
     user_id = ctx.author.id
+    data = await fetch_data(user_id, 'habits')
     intro = f"{random.choice(GREETINGS)} {ctx.author.mention},\n\n"
 
-    data = await fetch_data(user_id, 'habits')
-    if habit is None:
-        # TODO: No habit specified message
+    if habit is None:  # Comment
         await ctx.send(f"No habit specified")
-    elif not habit.strip():
-        # TODO: habit incorrect format message
+
+    elif not habit.strip():  # Comment
         await ctx.send(f"Habit has an incorrect format")
+
     elif len(data['habits']) >= HABIT_DEFAULT_MAX:
-        # TODO: Maximum habits reached message
         print(data['habits'])
         print(len(data['habits']))
         await ctx.send(f"Maximum habits reached")
+
     elif len(habit.strip()) > HABIT_CHAR_MAX:
         # TODO: Maximum character length message
         await ctx.send(f"Maximum character length exceeded")
-    else:
-        async with aiosqlite.connect("data/billy.db") as db:
-            if data is None:
+
+    else:  # Comment
+        async with aiosqlite.connect(DATABASE) as db:
+
+            if data is None:  # Comment
                 habits = [habit.lower()]
                 await db.execute(f"""INSERT INTO user_data (user_id, habits) VALUES ({user_id}, "{habits}");""")
-                # TODO: Create profile and add habit message
                 await ctx.send(f"Profile created and added your first habit")
-            else:
+
+            else:  # Comment
                 data['habits'].append(habit.lower())
                 habit_string = str(data['habits'])
                 await db.execute(f"""UPDATE user_data SET habits="{habit_string}" WHERE user_id={user_id};""")
                 # TODO: add habit message
                 await ctx.send(f"Habit added")
 
+            # Comment
             await db.commit()
 
 
 @client.command()
 async def remove_habit(ctx, *, habit_loc=None):
-    # TODO: Error handling, any input? has user got a profile? has user got any habits to delete?
+    """
+
+    :param ctx:
+    :param habit_loc:
+    :return:
+    """
+
+    # Comment
     user_id = ctx.author.id
     data = await fetch_data(user_id)
+
+    # Comment
     if habit_loc is None:
-        # TODO: habit to delete unspecified message
-        # This will check if anything was specified in habit_loc if no will trigger a no habit specified message
         await ctx.send(f'habit to delete unspecified')
+
+    # Comment
     elif data is None:
-        # TODO: no profile exists message
-        # This checks if there is a profile, if not it will trigger a no profile exists message
         await ctx.send(f'No profile exits')
+
+    # Comment
     elif len(data['habits']) == 0 or data['habits'] is None:
-        # TODO: no habits message
-        # This checks the string in 'habits' and if is empty or returns None then will trigger a no habits message
         await ctx.send(f'you dont have any habits')
+
+    # Comment
     elif not habit_loc.isdigit() and not isinstance(habit_loc, str):
-        # TODO: incorrect format message
-        # This should check if habit_loc is not a string or a potential integer and if not return
-        # incorrect format message
         await ctx.send(f"incorrect format")
+
+    # Comment
     elif habit_loc.lstrip('-').isdigit() and not 0 < int(habit_loc) <= len(data['habits']):
-        # TODO: outside scope messages
         await ctx.send(f'I am hitting this')
-        # if habit is at least 1 and maximum of length of 'habits' then:
+
+        # Comment
         if (int(habit_loc) <= 0 or int(habit_loc) > len(data['habits'])) and len(data['habits']) >= 2:
-            # checks if habit_loc is 0 or less, or if habit_loc is more than length of 'habits'.
-            # and also checks if length of 'habits' is more than 1 and then triggers a message
             await ctx.send(f"please choose a number between 1 and {len(data['habits'])}")
-        else:
-            # or if you only have 1 in length of 'habits' then:
+
+        else:  # Comment
             await ctx.send(f"you only have one habit")
+
+    # Comment
     elif isinstance(habit_loc, str) and habit_loc.lower() not in data['habits'] and not habit_loc.isdigit():
-        # then if habit_loc is a string and not an integer:
-        # TODO: cant find your habit message
-        # then if habit_loc is not in 'habits' send a message saying so:
         await ctx.send(f'habit cannot be found')
-    else:
-        async with aiosqlite.connect("data/billy.db") as db:
+
+    else:  # Comment
+
+        # Comment
+        async with aiosqlite.connect(DATABASE) as db:
+
+            # Comment
             if habit_loc.isdigit():
-                # If habit is an integer then it should convert the potential integer into an integer
                 habit_index = int(habit_loc) - 1
+
+            # Comment
             elif isinstance(habit_loc, str):
                 habit_index = data['habits'].index(habit_loc.lower())
-                # if habit_loc integer follows all the above then it needs to be formatted into an index by - 1
-                # assiign to habit_index variable and del the data from habits using that index
-            else:
+
+            else:  # Comment
                 print(f"unknown error")
+
+            # Comment
             del data['habits'][habit_index]
-            # connect to database, convert habits list to a string and uses SQL query to update database
             habit_string = str(data['habits'])
             await db.execute(f"""UPDATE user_data SET habits="{habit_string}" WHERE user_id={user_id};""")
-            # TODO: habit removed code and message
             await ctx.send(f'habit removed')
             await db.commit()
 
@@ -202,13 +219,15 @@ async def list_habits(ctx):
     user_id = ctx.author.id
     data = await fetch_data(user_id, 'habits')
 
-    if data is None:
-        # TODO: no profile exists message
+    if data is None:  # Comment
         await ctx.send("No profile exists")
+
+    # Comment
     elif len(data['habits']) == 0 or data['habits'] is None:
         # TODO: you have no habits message
         await ctx.send("You have no habits")
-    else:
+
+    else:  # Comment
         habit_list = []
 
         for i, habit in enumerate(data['habits']):
@@ -226,6 +245,7 @@ async def list_habits(ctx):
 
 @client.command(name='create_issue')
 async def create_issue(ctx, *, issue_input=None):
+
     # Formatting the issue and error handling
     intro = f"{random.choice(GREETINGS)} {ctx.author.mention},\n\n" \
             f"Thank you for attempting to notify us of a bug or potential future enhancement.\n\n" \
